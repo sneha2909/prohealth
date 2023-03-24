5fb9f3a4b2bcef257d06b54b9846488423bfcef6
unknown <sneha.kothi@somaiya.edu>
2023-03-24 11:55:13 +0530

    changes in workout_buddy

--- webapp/models.py
+++ webapp/models.py
@@ -7,13 +7,13 @@ class User(AbstractUser):
     username = models.CharField(max_length=255, unique=True, null=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    # phone_number=models.CharField(max_length=10)
-    # gender=models.CharField(max_length=20)
-    # email = models.EmailField()
+    phone_number=models.CharField(max_length=10,blank=True, null=True)
+    gender=models.CharField(max_length=20, null=True)
+    email = models.EmailField()
     # slug = AutoSlugField(populate_from='name')
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
-    # dob = models.DateField(max_length=8)
+    # USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = []
+    dob = models.DateField(max_length=8,null=True)
     # age = models.IntegerField() 
     # def __str__(self):
     #     today = date.today()
