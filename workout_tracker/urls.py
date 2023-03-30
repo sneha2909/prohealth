6d82619e402a169face1813e3ceb404478756dae
Rohan Ajmera <rohanajm@gmail.com>
2023-03-30 23:19:05 +0530

    workout tracker done

--- workout_tracker/urls.py
+++ workout_tracker/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('user/logout$', views.logout), # destroys user session
     path('', views.dashboard,name='workout-tracker'), # get dashboard
     path('workout/', views.new_workout,name='new-workout'), # get workout page / add workout
-    path('workout/(?P<id>\d*)/', views.workout,name='get-workout'), # get workout / update workout
+    path('workout(?P<id>\d*)/', views.workout,name='get-workout'), # get workout / update workout
     path('workout/exercise/(?P<id>\d*)/', views.exercise,name='add-exercise'),  # add exercise
     path('workout/complete(?P<id>\d*)/', views.complete_workout,name='complete-workout'), # complete workout
     path('workout/edit(?P<id>\d*)/', views.edit_workout,name='edit-workout'), # edit workout
