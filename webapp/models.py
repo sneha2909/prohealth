35048cfeb79d967d741b9f016da6681887927964
unknown <sneha.kothi@somaiya.edu>
2023-04-13 18:00:46 +0530

    dashboard added

--- webapp/models.py
+++ webapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from autoslug import AutoSlugField
-from dateutil.relativedelta import relativedelta
-from datetime import date
+# from autoslug import AutoSlugField
+# from dateutil.relativedelta import relativedelta
+# from datetime import date
+# from django.db import models
+# from datetime import date
+# from sqlalchemy import null
+from ckeditor.fields import RichTextField
 
 class User(AbstractUser):
     # username = models.CharField(max_length=255, unique=True, null=True)
@@ -27,3 +31,76 @@ class User(AbstractUser):
     def __str__(self) -> str:
             return self.name
 
+class User_Info(models.Model):
+    user_id = models.AutoField
+    user_gender = models.CharField(max_length=50,blank=False,null=False)
+    user_focus_area = models.CharField(max_length=100,blank=False,null=False)
+    user_name = models.CharField(max_length=500,blank=False,null=False)
+    user_email = models.CharField(max_length=100,blank=False,null=False)
+    user_age = models.IntegerField(default=0)
+    user_blood_group = models.CharField(max_length=50,blank=False,null=False)
+    user_height_ft = models.IntegerField(default=0)
+    user_height_in = models.IntegerField(default=0)
+    user_height = models.FloatField(default=0)
+    user_weight = models.FloatField(default=0)
+    user_activity_level = models.CharField(max_length=100,blank=False,null=False)
+    user_bmi = models.FloatField(default=0)
+    user_bmr = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user_email
+
+
+class User_Exercise_Info(models.Model):
+    user_id = models.AutoField
+    user_name = models.CharField(max_length=500,blank=False,null=False)
+    exercise_name = models.CharField(max_length=500,blank=False,null=False)
+    exercise_count = models.IntegerField(default=0)
+    exercise_duration = models.FloatField(default=0)
+    exercise_calorie_burnt = models.FloatField(default=0)
+    exercise_weight_loss = models.FloatField(default=0)
+    current_time = models.DateTimeField(blank=False,null=False)
+
+    def __str__(self):
+        return self.user_name
+
+class Playlist_Check(models.Model):
+    user_id = models.AutoField
+    user_email = models.CharField(max_length=100,blank=False,null=False)
+    exercise_jj = models.IntegerField(default=0)
+    exercise_ac = models.IntegerField(default=0)
+    exercise_kp = models.IntegerField(default=0)
+    exercise_sar = models.IntegerField(default=0)
+    exercise_squats = models.IntegerField(default=0)
+    exercise_bl = models.IntegerField(default=0)
+    exercise_cs = models.IntegerField(default=0)
+    current_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user_email
+
+class Diet_Menu(models.Model):
+    
+    diet_options = (
+    ('Choose' ,'Choose'),
+    ('Low Carb Diet' ,'Low Carb Diet'),
+    ('High Carb Diet','High Carb Diet'),
+    ('Keto Diet','Keto Diet'),
+    ('Balanced Diet','Balanced Diet'),
+    ('Zone Diet','Zone Diet'),
+    ('Depletion Diet','Depletion Diet')
+    )
+    
+    diet_food_options = (
+    ('Choose' ,'Choose'),
+    ('Vegetarian' ,'Vegetarian'),
+    ('Non Vegetarian' ,'Non Vegetarian')
+    )
+
+    diet_menu_id = models.AutoField
+    diet_choice =  models.CharField(max_length=50,choices = diet_options,default = diet_options[0][0],blank=False,null=False)
+    diet_food_choice  = models.CharField(max_length=50,choices = diet_food_options,default = diet_food_options[0][0],blank=False,null=False)
+    diet_breakfast =  RichTextField(blank=False,null=False)
+    diet_lunch = RichTextField(blank=False,null=False)
+    diet_dinner = RichTextField(blank=False,null=False)
+    diet_snacks = RichTextField(blank=False,null=False)
--- webapp/views.py
+++ webapp/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .models import User
 from django.contrib import messages
+from .models import User_Info,User_Exercise_Info,Playlist_Check,Diet_Menu
+import pandas as pd
 # Create your views here.
 def home(request):
 	return render(request,'webapp/home.html')
@@ -77,5 +79,44 @@ def logout_view(request):
 	logout(request)
 	return redirect('home')
 
+def dashboard(request):
 
+    if request.session['user_mail_id'] == False:
+        return redirect('home')
 
+    else:
+        user_playlist = Playlist_Check.objects.filter(user_email=request.session['user_mail_id']).first()
+        user_data = User_Info.objects.filter(user_email=request.session['user_mail_id']).first()
+        
+        return render(request,'dashboard.html',{'user_data': user_data, 'playlist': user_playlist})
+    
+def progress(request):
+
+    user_data = User_Info.objects.filter(user_email=request.session['user_mail_id']).first()
+    user_data_exercise = User_Exercise_Info.objects.filter(user_name=user_data.user_name)
+    playlist_status = Playlist_Check.objects.filter(user_email = request.session['user_mail_id'])
+
+    df = pd.DataFrame(None)
+    df=pd.DataFrame(user_data_exercise.values())
+    df["current_time"] = pd.to_datetime(df["current_time"]).dt.date
+    df = df.groupby(['current_time']).sum()
+
+    x_axis=df.index.tolist()
+    y_axis=df['exercise_calorie_burnt'].tolist()
+
+    y1_axis=df['exercise_weight_loss'].multiply(10000).tolist()
+
+    exercise_duration = df['exercise_duration'].sum().tolist()
+
+    calories_burnt = df['exercise_calorie_burnt'].sum().tolist()
+    
+    playlist_lt = [list(i.values()) for i in playlist_status.values()]
+
+    return render(request,'progress.html',{'user_data': user_data ,
+                                            'user_data_exercise':user_data_exercise,
+                                            'x_axis':x_axis,
+                                            'y_axis':y_axis,
+                                            'y1_axis':y1_axis,
+                                            'exercise_duration':round(exercise_duration/60,2),
+                                            'calories_burnt':round(calories_burnt,2),
+                                            'playlist_lt':playlist_lt[0][2:9]})
