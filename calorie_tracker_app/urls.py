dee367dc8ca855d4cb818095d6aa4023fefe9b67
Rohan Ajmera <rohanajm@gmail.com>
2023-05-03 17:09:34 +0530

    modified calorie tracker

--- calorie_tracker_app/urls.py
+++ calorie_tracker_app/urls.py
@@ -2,7 +2,7 @@ from calorie_tracker_app.apps import CalorieTrackerAppConfig
 from django.urls import path
 from . import views
 
-# app_name = "calorie_tracker_app"
+app_name = "calorie_tracker_app"
 
 urlpatterns = [
     path('', views.calorie_tracker, name="index"),
